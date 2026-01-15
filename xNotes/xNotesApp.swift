352ea9f37664b9949afbc5d66b8a3ae5bdcdc490
Dirk Clemens <git@adcore.de>
2026-01-15 07:24:16 +0100

    Initial Commit

---
+++ xNotes/xNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  xNotesApp.swift
+//  xNotes
+//
+//  Created by Dirk Clemens on 15.01.26.
+//
+
+import SwiftUI
+
+@main
+struct xNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
